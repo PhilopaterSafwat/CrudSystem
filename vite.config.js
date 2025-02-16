d3691d81a4756fdb45943ad033d5ef13ee30bbab
felo <felo.safwat2000@gmail.com>
2025-02-16 06:42:28 +0200

    Done

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://crudsystemapp.eu-4.evennode.com"
+      "/api": {
+        target: "http://crudsystemapp.eu-4.evennode.com",
+        changeOrigin: true
+      }
     }
   }
 })
